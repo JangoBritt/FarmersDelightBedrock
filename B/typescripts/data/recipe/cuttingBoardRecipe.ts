44c6db5c9c5bae02db7810272ae70a86e086ab0f
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2024-08-14 13:49:56 +0800

    fix:修复部分方块渲染翻转

--- B/typescripts/data/recipe/cuttingBoardRecipe.ts
+++ B/typescripts/data/recipe/cuttingBoardRecipe.ts
@@ -85,7 +85,9 @@ const BlockofPickaxeList: string[] = [
     "minecraft:amethyst_block",
     "minecraft:bricks",
     "minecraft:deepslate",
-    //"minecraft:quartz_block",
+    "minecraft:quartz_block",
+    "minecraft:quartz_block",
+    "minecraft:quartz_block",
     "minecraft:stone",
     "minecraft:nether_brick"
 ];
